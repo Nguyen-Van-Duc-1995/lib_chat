9725bf55d2eccd862a3bd4b15cdd1c0c18be4b85
Nguyen-Van-Duc-1995 <ducnguyen95hust@gmail.com>
2025-09-23 23:06:05 +0700

    realtime price

--- lib/chart_screen.dart
+++ lib/chart_screen.dart
@@ -330,6 +330,20 @@ class TradingViewModel extends ChangeNotifier {
               _klines[_klines.length - 1] = newKline;
             }
 
+            try {
+              _tickerData = TickerData(
+                symbol: data['Symbol'],
+                currentPrice: (data['LastPrice'] / 1000 as num).toDouble(),
+                priceChange: (data['Change'] / 1000 as num).toDouble(),
+                priceChangePercent: double.parse(data['RatioChange']),
+                volume24h: double.parse(data['TotalVol']),
+                high24h: double.parse(data['Highest']),
+                low24h: double.parse(data['Lowest']),
+              );
+            } catch (e) {
+              print('Error processing ticker data: $e');
+            }
+
             notifyListeners();
           } catch (e) {
             print('Error processing kline data: $e');
